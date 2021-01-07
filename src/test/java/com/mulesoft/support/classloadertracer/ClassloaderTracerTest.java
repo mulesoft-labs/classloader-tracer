8fc2063d4599e5b42e44f87629f778f051f3a491
Alejandro Dobniewski <alejandro.dobniewski@mulesoft.com>
2021-01-07 11:26:01 -0300

    Upgrade to JUnit 5. Fix src test directory.

---
+++ src/test/java/com/mulesoft/support/classloadertracer/ClassloaderTracerTest.java
@@ -0,0 +1,15 @@
+package com.mulesoft.support.classloadertracer;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Created by adobni on 14/Apr/16.
+ */
+public class ClassloaderTracerTest {
+    @Test
+    void test1() throws Exception {
+        // TODO implement test
+    }
+
+}
